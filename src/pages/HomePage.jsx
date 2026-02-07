9b9f0206daeba0f3d95dec9f8f3141a991c1dd93
Rahmatxonov <rahmatxonov1001@gmail.com>
2026-02-07 15:48:57 +0500

    home page changed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
-import ParentComponent from '../components/ParentComponent';
 
 function HomePage() {
   const [count, setCount] = useState(0);
